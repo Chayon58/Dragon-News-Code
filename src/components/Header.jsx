663737c0aa47dde28b2bad3afd8392beb7dd260d
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-03 22:52:43 +0600

    added img width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from '../assets/logo.png';
 const Header = () => {
     return (
-        <div className='flex justify-center'>
-            <img src={logo} alt="" />
+        <div className='flex justify-center flex-col items-center gap-3'> 
+            <img className='w-[450px]' src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
         </div>
     );
 };
