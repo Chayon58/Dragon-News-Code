2f91d1e5f415a209de2588d0ffc0caf1e6923cbf
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-03 23:18:06 +0600

    added month

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { format } from 'date-fns';
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'> 
             <img className='w-[450px]' src={logo} alt="" />
             <p className='font-bold text-accent'>Journalism Without Fear or Favour</p>
-            <p className='font-bold text-black'>{format(new Date(), 'EEEE ,')}</p>
+            <p className=' text-accent'>{format(new Date(), 'EEEE, MMMM MM, yyyy')}</p>
         </div>
     );
 };
