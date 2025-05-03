2b75a4200ada86bcda6daea5e54d13cfe14aa455
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-03 23:13:54 +0600

    added img date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'> 
             <img className='w-[450px]' src={logo} alt="" />
-            <p>Journalism Without Fear or Favour</p>
+            <p className='font-bold text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-bold text-black'>{format(new Date(), 'EEEE ,')}</p>
         </div>
     );
 };
