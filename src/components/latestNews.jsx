bc3e5965846f10f630648d76735ce8c789fc5f5a
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-04 22:13:03 +0600

    added marquee

--- src/components/latestNews.jsx
+++ src/components/latestNews.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
 
 const latestNews = () => {
     return (
-        <div>
-            latest
+        <div className='flex items-center gap-3 bg-base-200 p-3'>
+            <p className='text-base-100 bg-secondary px-3 py-2'>Latest</p>
+            <Marquee>
+            <p className='font-bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, numquam?</p>
+            </Marquee>
         </div>
     );
 };
--- src/layouts/Homelayout.jsx
+++ src/layouts/Homelayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
-
+import LatestNews from '../components/latestNews';
 const Homelayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
-                <section>
-                    <latestNews></latestNews>
+                <section className='w-11/12 mx-auto flex justify-between items-center my-3'>
+                    <LatestNews></LatestNews>   
                 </section>
             </header>
             <main>
