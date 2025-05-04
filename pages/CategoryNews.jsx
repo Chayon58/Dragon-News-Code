501d0ed7994ccba93599b22d66d44df14ab92706
Chayon Chandra Biswas <chayonbiswas8610@gmail.com>
2025-05-05 00:27:44 +0600

    fetch data

--- pages/CategoryNews.jsx
+++ pages/CategoryNews.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const { id } = useParams();
+    const data = useLoaderData();
+    // console.log(id,data);
+
+    
+
+    useEffect(() => {
+        const filterNews = data.filter((news) => news.category_id == id);
+        console.log(filterNews);
+    }, [data, id]);
+    
     return (
         <div>
-            CategoryNews
+            CategoryNews -{id}
         </div>
     );
 };
